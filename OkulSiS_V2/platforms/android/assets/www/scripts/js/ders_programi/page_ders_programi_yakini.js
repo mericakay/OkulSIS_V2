25bb3df9d177ab67aacdcb35adc63b0e56e8be84
mericakay <mericakay@gmail.com>
2017-10-30 17:43:05 +0300

    Ders Programı Yakın Bitti

---
+++ OkulSiS_V2/platforms/android/assets/www/scripts/js/ders_programi/page_ders_programi_yakini.js
@@ -0,0 +1,79 @@
+﻿function user() {
+    var did = localStorage.getItem("dyiliid");
+    var rolid = localStorage.getItem("RolID");
+    var okulid = localStorage.getItem("OkulID");
+    var kisiid = localStorage.getItem("gelenid");
+    var ip = localStorage.getItem("proxy");
+    var kisiadi = localStorage.getItem("KullaniciAdi");
+    document.getElementById("username_mrc").innerHTML = kisiadi;
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=mobilMenu_mbllogin&RolID=' + rolid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].MenuAdi;
+                var img = data[j].ImageURL;
+                var url = data[j].URL;
+                var value = data[j].value;
+                var iconcolor = data[j].iconcolor;
+
+                var selected = data[j].selected;
+                $('.sidebar-menu').append('<ul><li><a href="../../' + url + '"><i class="fa fa-circle-o text-' + iconcolor + '"></i> <span>' + text + '</span></a></li></ul>');
+            }
+        }
+    })
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=VeliOgrencileri_mbllogin&dersYiliID=' + did + '&kisiId=' + kisiid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            $('#selectNumber').empty();
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].AdiSoyadi;
+                var value = data[j].OgrenciID;
+                var ogrenciseviyeid = data[j].OgrenciSeviyeID;
+                var sinifid = data[j].SinifID;
+
+                $('#selectNumber').append("<option value=" + sinifid + ">" + text + "</option>");
+            }
+            $("#selectNumber").on('change', function () {
+                var ogrenciidselected = this.value;
+                alert(ogrenciidselected);
+                $.ajax({
+                    url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=OgrenciVeYakiniDersProgramiListesi_mbllogin&ogrenciSeviyeID='+ogrenciseviyeid+'&sinifID='+this.value+'&donemID=1',
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function (data) {
+                        var j;
+                        var dataSet = [];
+                        var properties = [];
+                        for (var j = 0; j < data.length; j++) {
+                            var dersadi = data[j].DersAdi;
+                            var sinifkodu = data[j].SinifKodu;
+                            var dersbaslangicbitis = data[j].DersBaslangicBitisSaati;
+                            var Tc = data[j].TCKimlikNo;
+                            var selected = data[j].selected;
+
+                            $('#location').append('<tr><td>' + dersadi + '</td><td>' + sinifkodu + '</td><td>' + dersbaslangicbitis + '</td></tr>');
+
+                        }
+                    }
+
+                });
+            });
+        }
+
+    });
+};
+
+
+
