ee6da9c3ae8e5a712ed1dc0d0d830a772cd12b3d
mericakay <mericakay@gmail.com>
2017-10-30 18:06:05 +0300

    Profil Tamam

---
+++ OkulSiS_V2/platforms/android/assets/www/scripts/js/profil/profil.js
@@ -0,0 +1,54 @@
+﻿function user() {
+    var did = localStorage.getItem("dyiliid");
+    var rolid = localStorage.getItem("RolID");
+    var okulid = localStorage.getItem("OkulID");
+    var kisiid = localStorage.getItem("gelenid");
+    var ip = localStorage.getItem("proxy");
+    var kisiadi = localStorage.getItem("KullaniciAdi");
+    document.getElementById("username_mrc").innerHTML = kisiadi;
+
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=mobilMenu_mbllogin&RolID=' + rolid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].MenuAdi;
+                var img = data[j].ImageURL;
+                var url = data[j].URL;
+                var value = data[j].value;
+                var iconcolor = data[j].iconcolor;
+                var selected = data[j].selected;
+                $('.sidebar-menu').append('<ul><li><a href="../../' + url + '"><i class="fa fa-circle-o text-' + iconcolor + '"></i> <span>' + text + '</span></a></li></ul>');
+            }
+        }
+    })
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=GnlProfil_mbllogin&kisiID='+kisiid+'',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+
+
+
+            var j;
+            var dataSet = [];
+            var properties = [];
+            for (var j = 0; j < data.length; j++) {
+                var tc = data[j].TCKimlikNo;
+                var adi = data[j].Adi;
+                var soyadi = data[j].Soyadi;
+                var eposta = data[j].ePosta;
+                $('#location').append('<tr><td>' + tc + '</td><td>' + adi + '</td><td>' + soyadi + '</td><td>' + eposta + '</td></tr>');
+            }
+        }
+
+    });
+};
+
+
