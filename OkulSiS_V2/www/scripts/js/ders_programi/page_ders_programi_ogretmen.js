0fe617ba9a7a6d31c472e854d7d6ebc9cce4d7ba
mericakay <mericakay@gmail.com>
2017-10-30 17:15:19 +0300

    ders programi ögretmen

--- OkulSiS_V2/www/scripts/js/ders_programi/page_ders_programi_ogretmen.js
+++ OkulSiS_V2/www/scripts/js/ders_programi/page_ders_programi_ogretmen.js
@@ -1,1 +1,56 @@
-﻿
+﻿function user() {
+    var did = localStorage.getItem("dyiliid");
+    var rolid = localStorage.getItem("RolID");
+    var okulid = localStorage.getItem("OkulID");
+    var kisiid = localStorage.getItem("gelenid");
+    var ip = localStorage.getItem("proxy");
+    var kisiadi = localStorage.getItem("KullaniciAdi");
+    document.getElementById("username_mrc").innerHTML = kisiadi;
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=mobilMenu_mbllogin&RolID=' + rolid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].MenuAdi;
+                var img = data[j].ImageURL;
+                var url = data[j].URL;
+                var value = data[j].value;
+                var iconcolor = data[j].iconcolor;
+
+                var selected = data[j].selected;
+                $('.sidebar-menu').append('<ul><li><a href="../../' + url + '"><i class="fa fa-circle-o text-' + iconcolor + '"></i> <span>' + text + '</span></a></li></ul>');
+            }
+        }
+    })
+
+    $.ajax({
+
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=OgretmenDersProgramiListesi_mbllogin&ogretmenID='+kisiid+'&dersYiliID='+did+'&donemID=1',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            //$('#location').empty();
+            for (var j = 0; j < data.length; j++) {
+                var dersadi = data[j].DersAdi;
+                var sinifkodu = data[j].SinifKodu;
+                var dersbaslangicbitis = data[j].DersBaslangicBitisSaati;
+                var Tc = data[j].TCKimlikNo;
+                var selected = data[j].selected;
+
+                $('#location').append('<tr><td>' + dersadi + '</td><td>' + sinifkodu + '</td><td>' + dersbaslangicbitis + '</td></tr>');
+            
+            }
+
+        }
+    });
+};
+
+
