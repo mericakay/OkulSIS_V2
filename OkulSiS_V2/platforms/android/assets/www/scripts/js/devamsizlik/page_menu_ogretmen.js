8a66db80ab16a0780e2e0534bb4f526cdd54e0ae
mericakay <mericakay@gmail.com>
2017-10-30 15:41:08 +0300

    ders_listesi Düzenleme JS

--- OkulSiS_V2/platforms/android/assets/www/scripts/js/devamsizlik/page_menu_ogretmen.js
+++ OkulSiS_V2/platforms/android/assets/www/scripts/js/devamsizlik/page_menu_ogretmen.js
@@ -1,11 +1,36 @@
-﻿function load() {
-
+﻿function user() {
+    var did = localStorage.getItem("dyiliid");
+    var rolid = localStorage.getItem("RolID");
     var okulid = localStorage.getItem("OkulID");
     var kisiid = localStorage.getItem("gelenid");
     var ip = localStorage.getItem("proxy");
+    var kisiadi = localStorage.getItem("KullaniciAdi");
+    document.getElementById("username_mrc").innerHTML = kisiadi;
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=mobilMenu_mbllogin&RolID=' + rolid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].MenuAdi;
+                var img = data[j].ImageURL;
+                var url = data[j].URL;
+                var value = data[j].value;
+                var iconcolor = data[j].iconcolor;
+
+                var selected = data[j].selected;
+                $('.sidebar-menu').append('<ul><li><a href="../../' + url + '"><i class="fa fa-circle-o text-' + iconcolor + '"></i> <span>' + text + '</span></a></li></ul>');
+            }
+        }
+    })
+
     $.ajax({
 
-        url: 'http://'+ip+':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=ogretmenDersProgrami_mbllogin&kisiId=' + kisiid + '&OkulID=' + okulid + '',
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=ogretmenDersProgrami_mbllogin&kisiId=' + kisiid + '&OkulID=' + okulid + '&dersYiliID=9D7A115C-5E96-4F6E-B31D-E5710BDA1C97',
         type: 'GET',
         dataType: 'json',
         success: function (data) {
@@ -16,12 +41,13 @@
             for (var j = 0; j < data.length; j++) {
                 var text = data[j].Aciklama;
                 var sinifid = data[j].SinifID;
+                // alert(sinifid);
                 $('#selectNumber').append("<option value=" + sinifid + ">" + text + "</option>");
             }
             $("#selectNumber").on('change', function () {
-                alert($(this).val());
+                // alert($(this).val());
                 var x = document.getElementById("myDate").value;
-                alert(x);
+                // alert(x);
                 if (x === "") {
                     alert("Lütfen Tarih Seçiniz !!")
 
@@ -74,6 +100,6 @@
             });
         }
     });
+};
 
 
-}
---
+++ OkulSiS_V2/platforms/android/assets/www/scripts/js/mesaj/page_mesaj_ogretmen.js
@@ -0,0 +1,54 @@
+﻿function user() {
+    var did = localStorage.getItem("dyiliid");
+    var rolid = localStorage.getItem("RolID");
+    var okulid = localStorage.getItem("OkulID");
+    var kisiid = localStorage.getItem("gelenid");
+    var ip = localStorage.getItem("proxy");
+    var kisiadi = localStorage.getItem("KullaniciAdi");
+    document.getElementById("username_mrc").innerHTML = kisiadi;
+
+    $.ajax({
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=mobilMenu_mbllogin&RolID=' + rolid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            for (var j = 0; j < data.length; j++) {
+                var text = data[j].MenuAdi;
+                var img = data[j].ImageURL;
+                var url = data[j].URL;
+                var value = data[j].value;
+                var iconcolor = data[j].iconcolor;
+
+                var selected = data[j].selected;
+                $('.sidebar-menu').append('<ul><li><a href="../../' + url + '"><i class="fa fa-circle-o text-' + iconcolor + '"></i> <span>' + text + '</span></a></li></ul>');
+            }
+        }
+    })
+
+    $.ajax({
+
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?url=GelenMesajListesi_mbllogin&kisiID=' + kisiid + '',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var j;
+            var dataSet = [];
+            var properties = [];
+            //$('#location').empty();
+            for (var j = 0; j < data.length; j++) {
+                var tarih = data[j].Tarih;
+                var Adi = data[j].SenderAdiSoyadi;
+                var konu = data[j].Konu;
+                var mesaj = data[j].Mesaj;
+                var selected = data[j].selected;
+                $('#location').append('<tr><td>' + Adi + '</td><td>' + konu + '</td><td>' + mesaj + '</td><td>' + tarih + '</td></tr>');
+            }
+
+        }
+    });;
+};
+
+
