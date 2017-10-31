a66a52aadfeb5ca3cfe68405cdc20b7302f1db49
mericakay <mericakay@gmail.com>
2017-10-31 18:39:18 +0300

    sınav listesi first

--- OkulSiS_V2/www/scripts/selectSchool.js
+++ OkulSiS_V2/www/scripts/selectSchool.js
@@ -6,7 +6,7 @@
    // alert(tc);
     $("#selectNumber")[0].selectedIndex = -1
     $.ajax({
-        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?tc=' + tc + '&url=mobilfirstdata_mbllogin&cid=' + cid + '',
+        url: 'http://' + ip + ':8080/Slim_Proxy_okulsis/SlimProxyBoot.php?tc='+tc+'&url=mobilfirstdata_mbllogin&cid='+cid+'',
         type: 'GET',
         dataType: 'json',
         success: function (data) {
@@ -19,22 +19,22 @@
                 var text = data[j].OkulAdi;
                 var okulid = data[j].OkulID;
                 var dersyiliid = data[j].DersYiliID;
-                rolid = data[j].RolID;
                 var dbn = data[j].dbn;
                
+                rolid = data[j].RolID;
 
-                $('#selectNumber').append("<option id=" + dbn + "  value=" + RolID + ">" + text + "</option>");
+                $('#selectNumber').append("<option class=" + dbn + "  value=" + rolid + ">" + text + "</option>");
 
             }
             $("#selectNumber").on('change', function () {
-              
-                localStorage.setItem("dbn", $(this).find('option:selected').attr('id'));
                 localStorage.setItem("RolID", $(this).find('option:selected').attr('value'));
+                localStorage.setItem("dbn", $(this).find('option:selected').attr('class'));
                 localStorage.setItem("OkulID", okulid);
                 localStorage.setItem("dyiliid", dersyiliid);
-                var dbn = localStorage.getItem("dbn");
-                var rolid = localStorage.getItem("RolID");
+                //var dbn = localStorage.getItem("dbn")
+               // alert(dbn);
                 window.location.href = "index2.html";
+                // alert(dersyiliid);
             });
         }
 
